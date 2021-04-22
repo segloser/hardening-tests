7176e2d30c2ebc79347d6323e5b8a60f28d9f6ea
Segloser <12746414+segloser@users.noreply.github.com>
2021-04-22 14:16:20 +0200

    Update wazuh-agent-install-hardening.sh

--- wazuh-agent-install-hardening.sh
+++ wazuh-agent-install-hardening.sh
@@ -11,54 +11,84 @@
 ## [w] - Warning!
 
 LOG="/tmp/wazuh-install.log"
+echo $(date) >> $LOG
+echo "============================="
 
-clear
-# Run me as sudo 
-if [[ $EUID -ne 0 ]]
-then
+VERSIONCHECK=$(cat /etc/os-release  | grep VERSION_ID | grep -Eo "[0-9]{2}.[0-9]{2}")
+function check_ubuntu_version(){
+
+	if [[ $VERSIONCHECK == "20.04" ]]
+	then
+		VERSION="20"
+	elif [[ $VERSIONCHECK == "18.04" ]]
+	then
+		VERSION="18"
+	else
+		echo "This script is not tested for the detected version"
+	fi
+}
+check_ubuntu_version
+
+function sudo_check(){
 	clear
-	echo "[i] - This script must be run as root (sudo)"
-	echo "[i] - Exiting now."
-	exit 1
-fi
+	# Run me as sudo 
+	if [[ $EUID -ne 0 ]]
+	then
+		clear
+		echo "[i] - This script must be run as root (sudo)"
+		echo "[i] - Exiting now."
+		exit 1
+	fi
+}
+sudo_check
 
 # Temporarily disabling unattended upgrades
-echo "Disabling Unattended-Upgrades"
-sed -i 's/APT::Periodic::Unattended-Upgrade "1"/APT::Periodic::Unattended-Upgrade "0"/g' /etc/apt/apt.conf.d/20auto-upgrades
+function update_disabling(){
+	echo "Disabling Unattended-Upgrades"
+	sed -i 's/APT::Periodic::Unattended-Upgrade "1"/APT::Periodic::Unattended-Upgrade "0"/g' /etc/apt/apt.conf.d/20auto-upgrades
 
-echo "Killing updating processes, to unlock update functionalities for the script"
-ps aux | grep "/usr/lib/apt/apt.systemd.daily" | grep -v grep| awk {'print $2'} > /tmp/tokill
-ps aux | grep "/run/mlocate.daily.lock" | grep -v grep| awk {'print $2'} >> /tmp/tokill
-ps aux | grep "apt-check" | grep -v grep| awk {'print $2'} >> /tmp/tokill
+	echo "Killing updating processes, to unlock update functionalities for the script"
+	ps aux | grep "/usr/lib/apt/apt.systemd.daily" | grep -v grep| awk {'print $2'} > /tmp/tokill
+	ps aux | grep "/run/mlocate.daily.lock" | grep -v grep| awk {'print $2'} >> /tmp/tokill
+	ps aux | grep "apt-check" | grep -v grep| awk {'print $2'} >> /tmp/tokill
 
-for PID in $(cat /tmp/tokill)
-do
-	sudo kill $PID
-done
+	for PID in $(cat /tmp/tokill)
+	do
+		sudo kill $PID
+	done
 
-sudo rm /var/cache/apt/archives/lock
-sudo rm /var/lib/dpkg/lock
-sudo rm /var/lib/dpkg/lock-frontend
-read -p "Lock files removed - Press ENTER to start"
+	sudo rm /var/cache/apt/archives/lock
+	sudo rm /var/lib/dpkg/lock
+	sudo rm /var/lib/dpkg/lock-frontend
+	read -p "Lock files removed - Press ENTER to start"
+}
+update_disabling
 
 # First apt update
-clear
-echo "[i] - As this should be a clean Ubuntu installation, I need to update..."
-echo "[i] - Checking Internet connection..."
-if [[ $(ping -c1 1.1.1.1 | grep -Eo "100% packet loss") == "100% packet loss" ]]
-then
-	echo "[i] - No Internet connection."
-	echo "[i] - Please, fix the connectivity issue and try again."
-	echo "[i] - Exiting now."
-	exit 1
-else
-	echo "[i] - Updating the system (no upgrading)"
-	sudo apt update -y
-fi
+function check_internet(){
+	clear
+	echo "[i] - As this should be a clean Ubuntu installation, I need to update..."
+	echo "[i] - Checking Internet connection..."
+	if [[ $(ping -c1 1.1.1.1 | grep -Eo "100% packet loss") == "100% packet loss" ]]
+	then
+		echo "[i] - No Internet connection."
+		echo "[i] - Please, fix the connectivity issue and try again."
+		echo "[i] - Exiting now."
+		exit 1
+	else
+		echo "[i] - Ready to update the system."
+	fi
+}
 
-# Purging Amazon Launcher
-echo "[i] - Removing the Amazon Launcher"
-sudo apt purge ubuntu-web-launchers -y
+check_internet
+sudo apt update -y
+
+# Purging Amazon Launcher in Ubuntu 18.04
+if [[ $VERSION == "18" ]]
+then
+	echo "[i] - Removing the Amazon Launcher"
+	sudo apt purge ubuntu-web-launchers -y
+fi
 
 # Installing curl
 if [[ $(dpkg -s curl | grep -Eo "Status: install ok installed") == "Status: install ok installed" ]]
@@ -73,31 +103,37 @@ fi
 
 clear
 # Wazuh GPG Key
-if [[ $(apt-key | grep -Eo "Wazuh.com") == "Wazuh.com" ]]
-then
-	echo "[i] - Wazuh GPG Key is already installed"
-else
-	echo "[i] - Installing Wazuh GPG Key..."
-	echo "================================="
-	sudo curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
-fi
+function install_wazuh_key(){
+	
+	if [[ $(apt-key | grep -Eo "Wazuh.com") == "Wazuh.com" ]]
+	then
+		echo "[i] - Wazuh GPG Key is already installed"
+	else
+		echo "[i] - Installing Wazuh GPG Key..."
+		echo "================================="
+		sudo curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
+	fi
 
-# Wazuh sources copied into sources.list
-if [[ $(cat /etc/apt/sources.list.d/wazuh.list | grep -Eo "deb https://packages.wazuh.com/4.x/apt/ stable main") == "deb https://packages.wazuh.com/4.x/apt/ stable main" ]]
-then
-	echo "[i] - sources.listd/.wazuh.list already exists"
-else
-	echo "[i] - Copying wazuh.list in /etc/apt/sources.list.d/"
-	echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
-fi
+	# Wazuh sources copied into sources.list
+	if [[ $(cat /etc/apt/sources.list.d/wazuh.list | grep -Eo "deb https://packages.wazuh.com/4.x/apt/ stable main") == "deb https://packages.wazuh.com/4.x/apt/ stable main" ]]
+	then
+		echo "[i] - sources.listd/.wazuh.list already exists"
+	else
+		echo "[i] - Copying wazuh.list in /etc/apt/sources.list.d/"
+		echo "deb https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+	fi
 
-sleep 2
-clear
-echo "[i] - Updating the system..."
-sudo apt update
+	sleep 2
+	clear
+	echo "[i] - Updating the system..."
+	sudo apt update
+}
+
+install_wazuh_key
 
 # Defining the Wazuh apt install command function
 function wazuh-install () {
+
 	echo "[i] - Installting the Wazuh agent"
 	sudo apt install wazuh-agent
 	echo
@@ -147,19 +183,20 @@ fi
 
 #
 function cis_download(){
+	
 	echo
-	echo "[i] - We need to download the CIS Benchmark Ubuntu 18.04 L1"
+	echo "[i] - We need to download the CIS SCA Benchmark for Ubuntu $VERSION.04 L1"
 	echo "[i] - This file should be provided by a remote or local security server"
-	echo "[r] - Press ENTER to download the cis_ubuntu18-04_L1.yml file from its default location or add a new URL now): "
+	echo "[r] - Press ENTER to download the cis_ubuntu$VERSION-04_L1.yml file from its default location or add a new URL now): "
 	read CISURL
 	cd /var/ossec/ruleset/sca
 
 	if [[ $CISURL == "" ]]
 	then
-		CISURL="https://raw.githubusercontent.com/segloser/hardening-tests/main/cis_ubuntu18-04_L1.yml"
+		CISURL="https://raw.githubusercontent.com/segloser/hardening-tests/main/cis_ubuntu$VERSION-04_L1.yml"
 	fi
 	wget $CISURL 
-	chgrp ossec /var/ossec/ruleset/sca/cis_ubuntu18-04_L1.yml
+	chgrp ossec /var/ossec/ruleset/sca/cis_ubuntu$VERSION-04_L1.yml
 	echo 'CISDOWNLOADED' >> $LOG
 }
 
@@ -167,17 +204,23 @@ if [[ $(grep -Eo "CISDOWNLOADED" $LOG) == "" ]]
 then 
 	cis_download
 	rm /var/ossec/ruleset/sca/debian*.yml
+else
+	echo "CIS SCA for Ubuntu $VERSION.04 already downloaded"
 fi
 
 # Installing SSH to implement a secure configuration.
 # It will be uninstalled.
+echo "Installing SSH"
+echo "=============="
 sudo apt install -y ssh
 
 sleep 2
 clear
 echo 
 echo "************************************************************************"
-echo "* Ready to install Ansible. This includes installing Python 2.7        *"  
+echo "* Ready to install Ansible. This includes installing Python 2.7        *"
+echo "* in case of Ubuntu $VERSION.04                                        *"
+echo "*                                                                      *"  
 echo "* Remember to unsinstall Python 2.7 when you finish with Ansible + CIS *"
 echo "************************************************************************"
 read -p "Press ENTER when ready"
@@ -187,17 +230,16 @@ function alter_ansible_install(){
 	rm -rf /tmp/ansible-alternative 
 	mkdir /tmp/ansible-alternative
 	cd /tmp/ansible-alternative
-	wget  https://github.com/segloser/hardening-tests/raw/main/ansible_ubuntu_18-04.zip
-	unzip ansible_ubuntu_18-04.zip
+	wget  https://github.com/segloser/hardening-tests/raw/main/ansible_ubuntu_$VERSION-04.zip -O /tmp/ansible-alternative/ansible.zip
+	unzip ansible.zip
 	sudo dpkg -i ./*.deb
-	echo "[i] - Ansible should have been installed from the deb packages downloaded from Segloser repository in GitHub."
+	echo "[i] - DEB packages installation process finished."
 	echo "[i] - Checking installation..."
 	if [[ $(dpkg -s ansible | grep -Eo "Status: install ok installed") == "Status: install ok installed" ]]
 	then
 		echo "[i] - Ansible is installed."
 		ANSVARINSTALL="dpkg"
 	else
-		clear
 		echo "[w] - Check why ansible cannot be installed."
 		echo "[i] - Exiting the program now."
 		exit 1
@@ -206,10 +248,13 @@ function alter_ansible_install(){
 
 function ansi_install(){
 
+	clear
+	echo "Trying to install ansible via: apt install ansible -y"
 	sudo apt install ansible -y
 
 	if [[ $(dpkg -s ansible | grep -Eo "Status: install ok installed") != "Status: install ok installed" ]]
 	then
+		echo "Conventional installation failed. Trying alternative cached packages installation"
 		alter_ansible_install
 	else
 		echo "[i] - Ansible seems to be properly installed."
@@ -219,18 +264,23 @@ function ansi_install(){
 
 ansi_install 
 
-if [[ $(grep -Eo "GITINSTALLED" $LOG) == "" ]]
-then 
-	echo
-	echo "[i] - Installing git"
-	sudo apt install git -y
-	echo "GITINSTALLED" >> $LOG
-fi
+function install_git(){
+	
+	if [[ $(grep -Eo "GITINSTALLED" $LOG) == "" ]]
+	then 
+		echo
+		echo "[i] - Installing git"
+		sudo apt install git -y
+		echo "GITINSTALLED" >> $LOG
+	fi
 
-if [ ! -d "/opt/Ubuntu1804-CIS" ]
-then
-	git clone https://github.com/florianutz/Ubuntu1804-CIS /opt/Ubuntu1804-CIS
-fi
+	if [ ! -d "/opt/Ubuntu1804-CIS" ]
+	then
+		git clone https://github.com/florianutz/Ubuntu1804-CIS /opt/Ubuntu1804-CIS
+	fi
+}
+
+install_git
 
 cd /opt/Ubuntu1804-CIS
 
@@ -254,15 +304,20 @@ sed -i 's/ubuntu1804cis_xwindows_required: false/ubuntu1804cis_xwindows_required
 sed -i 's/ubuntu1804cis_selinux_disable: false/ubuntu1804cis_selinux_disable: true/g' /opt/Ubuntu1804-CIS/roles/Ubuntu1804-CIS/defaults/main.yml
 sed -i 's/ubuntu1804cis_config_aide: true/ubuntu1804cis_config_aide: false/g' /opt/Ubuntu1804-CIS/roles/Ubuntu1804-CIS/defaults/main.yml
 
+echo "Generating the playbook (my_console.yml)"
 echo "- name: Harden Server" > /opt/Ubuntu1804-CIS/my_console.yml
 echo "  hosts: localhost" >> /opt/Ubuntu1804-CIS/my_console.yml
 echo "  become: yes" >> /opt/Ubuntu1804-CIS/my_console.yml
 echo ""
+echo "  any_errors_fatal: false" >> /opt/Ubuntu1804-CIS/my_console.yml
+echo "  ignore_errors: true" >> /opt/Ubuntu1804-CIS/my_console.yml
 echo "  roles:" >> /opt/Ubuntu1804-CIS/my_console.yml
 echo "    - Ubuntu1804-CIS" >> /opt/Ubuntu1804-CIS/my_console.yml
 
 sed -i 's/- name: generate new grub config\n  become: true\n  command: grub-mkconfig -o "{{ grub_cfg.stat.path }}"\n  notify: fix permissions after generate new grub config handler\n/- name: generate new grub config\n  become: true\n  command: grub-mkconfig -o "{{ grub_cfg.stat.path }}"\n  notify: fix permissions after generate new grub config handler\n  tags: grub_config\n/g'  /opt/Ubuntu1804-CIS/roles/Ubuntu1804-CIS/handlers/main.yml
 
+## Function to add a tag in a specific paragraph
+## Disabled by default. Only here just in case.
 function add_tag(){
 	echo '#!/user/bin/python' > /tmp/replace.py
 	echo 'import re' >> /tmp/replace.py
@@ -292,7 +347,7 @@ function add_tag(){
 	echo '    f.truncate()' >> /tmp/replace.py
 	python /tmp/replace.py
 }
-add_tag
+#add_tag
 
 # Installing auditd to implement a secure configuration
 sudo apt install -y auditd
@@ -301,19 +356,7 @@ clear
 
 # Skipping some rules and actions 
 read -p "[r] - Press ENTER when ready to play the book with Ansible"
-ansible-playbook /opt/Ubuntu1804-CIS/my_console.yml --skip-tags "aide, \
-rule_1.1.2, rule_1.1.3, rule_1.1.4, rule_1.1.5, rule_1.1.22, rule_1.5.1, \
-rule_1.6.1, rule_1.6.3, rule_1.6.4, rule_1.9, rule_2.1.6, rule_2.1.7, \
-rule_2.1.8, rule_2.1.9, rule_2.1.10, rule_2.1.11, rule_2.2.1.1, \
-rule_2.2.3, rule_2.2.4, rule_2.2.5, rule_2.2.6, rule_2.2.7, rule_2.2.8, \
-rule_2.2.9, rule_2.2.10, rule_2.2.11, rule_2.2.12, rule_2.2.13, \
-rule_2.2.14, rule_2.2.15, rule_2.2.16,rule_2.2.17, rule_2.3.1, \
-rule_2.3.2,rule_2.3.3,rule_2.3.4,rule_2.3.5, rule_4.1.1.1, prelim_tasks, \
-rule_1.1.1.4, rule_1.1.1.5, rule_1.9, rule_2.2.1.3, rule_5.2.1, rule_5.2.4, \
-rule_5.2.5, rule_5.4.1.5, rule_5.4.2, rule_6.2.1, rule_6.2.5, rule_6.2.8, \
-rule_6.2.11, rule_6.2.12, rule_6.2.14, grub_config"
-
-#, rule_4.1.1.2, rule_4.1.3
+ansible-playbook /opt/Ubuntu1804-CIS/my_console.yml --skip-tags "aide, grub_config"
 
 function ansible_remove(){
 	
@@ -357,6 +400,16 @@ function ansible_remove(){
 	fi
 }
 
+function restart_wazuh_agent(){
+	if [[ $(grep -Eo "MANAGER_IP" /var/ossec/etc/ossec.conf) != "MANAGER_IP" ]]
+	then
+		service wazuh-agent restart
+	else
+		echo "Check your Wazuh Agent configuration. There is no MANAGER_IP properly set in the ossec.conf file."
+		echo "You can check ossec.conf file in this directory: /var/ossec/etc"
+	fi
+}
+
 ## Ansible removal is disabled by default for the tests.
 ## Uncomment when ready to build your hardened template.
 #ansible_remove
@@ -368,14 +421,8 @@ function removing_misc(){
 	# Clean the ruleset/sca directory and restart Wazuh
 	[ -d /var/ossec/ruleset/sca/DISABLED  ] || mkdir /var/ossec/ruleset/sca/DISABLED
 	[ -f /var/ossec/ruleset/sca/cis_debian10.yml ] && mv /var/ossec/ruleset/sca/cis_debian10.yml /var/ossec/ruleset/sca/DISABLED
-
-	if [[ $(grep -Eo "MANAGER_IP" /var/ossec/etc/ossec.conf) != "MANAGER_IP" ]]
-	then
-		service wazuh-agent restart
-	else
-		echo "Check your Wazuh Agent configuration. There is no MANAGER_IP properly set in the ossec.conf file."
-		echo "You can check ossec.conf file in this directory: /var/ossec/etc"
-	fi
+	
+	restart_wazuh_agent
 
 	# Restoring unattended upgrades
 	sed -i 's/APT::Periodic::Unattended-Upgrade "0"/APT::Periodic::Unattended-Upgrade "1"/g' /etc/apt/apt.conf.d/20auto-upgrades
